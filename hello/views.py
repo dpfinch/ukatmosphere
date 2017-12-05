755e7e2b0a6e5d9924a4f34899f0c78d0b89a595
dpfinch <d.finch@ed.ac.uk>
2017-12-05 10:01:18 +0000

    Added a work in progress message

--- hello/views.py
+++ hello/views.py
@@ -8,7 +8,7 @@ def index(request):
     # return HttpResponse('Hello from Python!')
     r = requests.get('http://httpbin.org/status/418')
     print(r.text)
-    return HttpResponse('<prep>HAAAAPPPPPYYYY BBBIIIIRRRRTTTHHHHDDDAAAYYY KAT!!!!!!!</prep>')
+    return HttpResponse('<prep>This is the wesbite for UKAtmosphere. As you can see, its a work in progress.</prep>')
 
 
 def db(request):
