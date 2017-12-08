748af42cf4eccd767e7317097390446ebaa428b7
dpfinch <d.finch@ed.ac.uk>
2017-12-08 15:51:38 +0000

    More changes to try and get it to work

--- UKAsite/urls.py
+++ UKAsite/urls.py
@@ -15,12 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-# Added a bit of my own to see if it make things work...
-import polls.views
+
 
 urlpatterns = [
-    # trying to sort out the friggin home page
-    path(r'^$', polls.views.index, name = 'index'),
+    path('', include('polls.urls')),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.template import loader
 
 from .models import Question
@@ -12,8 +12,11 @@ def index(request):
     return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    try:
+        question = Question.objects.get(pk = question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question' : question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
