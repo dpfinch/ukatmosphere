cfd738aa02c2ae2c715a923fc73af40ed9c20b33
dpfinch <d.finch@ed.ac.uk>
2017-12-08 15:21:58 +0000

    Added a first index template

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
+from .models import Question
 # Create your views here.
 
 def index(request):
-    return HttpResponse("This is a tester thing for Django.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {'latest_question_list' : latest_question_list,}
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
     response = "You're looking at the results of question %s."
