916b2f0017a4bd9b10a8c1f1e7a6071fc081605e
dpfinch <d.finch@ed.ac.uk>
2017-12-11 13:57:49 +0000

    Added run line to Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn UKAsite.wsgi --log-file -
--- UKAsite/settings.py
+++ UKAsite/settings.py
@@ -14,7 +14,6 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -119,11 +118,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIR = (os.path.join(PROJECT_ROOT, 'static'))
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Database URL commands (taken from Heroku docs):
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+db_from_env =  dj_database_url.config(default = 'postgres://xpkexvfzlmezoi:fe7f3285c91a25690c45684573c6602fe2dddbb8d2b9b2eb056864cd8441f4d1@ec2-46-137-94-97.eu-west-1.compute.amazonaws.com:5432/d4k0p8vf123cm8')
+DATABASES['default'].update(db_from_env)
