ef750b11ed234153fdbb5f21baeb42198e0652bd
dpfinch <d.finch@ed.ac.uk>
2019-03-11 12:13:35 +0000

    Changes to EO Lessons page

--- UKAsite/worker.py
+++ UKAsite/worker.py
@@ -1,15 +1,41 @@
-# import os
-# from datetime import datetime as dt
-# import pandas
-# from dataplot.models import measurement_data, measurement_info, site_info
+import schedule
+from dataplot.models import *
+from dataplot import AQ_data_into_db as db
+from datetime import datetime as dt
+#### Need to schedule jobs to load new data into database.
+#### Start it at once per day. Possible to do more frequent later.
+
+def latest_AURN_station_data():
+    all_avail_sites = site_info.objects.filter(site_type = 'DEFRA AURN').filter(
+        site_open = True).values_list('site_name')
+    sites = [x[0] for x in all_avail_sites]
+    sites.sort()
+    year = dt.now().year
+
+    ## Catch if its a new year and get the previous year
+    if dt.now().month == 1 and dt.now().day == 1:
+        year = year - 1
+
+    for site in sites:
+        print(site)
+        db.Get_Latest_AURN_Data(site,year)
+
+def update_AURN_verified():
+    all_avail_sites = site_info.objects.filter(site_type = 'DEFRA AURN').values_list('site_name')
+    sites = [x[0] for x in all_avail_sites]
+
+    for site in sites:
+        db.Update_DEFRA_Data(site)
 
 
-### Need to have a table to have all the information about the sites
-### Won't be updated often but will be every now and again
-### Will need some sort of method to update it automatically when needed
+if __name__ == '__main__':
+    ## Get the latest data - set this to 18:00 - for no particular reason
+    schedule.every().day.at("18:00").do(latest_AURN_station_data)
 
-### The same will apply to the measurement info database. Need to be careful
-### about subtly different measurement ids, eg different unit
+    ## Check the verified data every monday at 01:00 - again no particular reason
+    schedule.every().monday.at("01:00").do(latest_AURN_station_data)
 
-### The measurement data table will do most of the hard work. It will need
-### to be constantly updated from lots of different sources
+
+    while True:
+
+        schedule.run_pending()
--- dataplot/layouts_EO_lessons.py
+++ dataplot/layouts_EO_lessons.py
@@ -10,12 +10,15 @@ def EO_Lessons():
     page_layout = html.Div(id ='full_page_container', children =
     [
     html.Div(className = 'page-header', children = [
-        html.Div(id = 'home-logo-holder', children = [html.A(id = 'home-logo', href="/")]),
-        html.Div(id = 'page-header-holder', children = [html.A('UK Atmosphere',id = "page-header-text", href = "/")]),
+        html.Div(id = 'home-logo-holder', children = [html.A(id = 'eo-pi-logo', href="/dataplot/EO_Lessons")]),
+        html.Div(id = 'page-header-holder', children = [html.A('Earth Observation',id = "page-header-text", href = "/dataplot/EO_Lessons")]),
     ]),
     html.Div(className = 'page-body',children = [
-    html.H1(id = 'UploadHolder', children = 'This is where the user can upload their own csv.'),
+    html.H1(id = 'UploadHolder', children = 'Choose a lesson...'),
     html.Br(),
+    html.P(id = 'EO_lessons_intro', children = [
+        'Earth observation is good.'
+    ])
     ])
     ])
     return page_layout
