61c317ef9112dce61ae3210dcc6b6df07860e3c6
dpfinch <d.finch@ed.ac.uk>
2019-03-11 13:57:38 +0000

    EO lessons test scripts 3

--- dataplot/layouts_EO_lessons.py
+++ dataplot/layouts_EO_lessons.py
@@ -7,6 +7,10 @@ from dataplot.DataTools import LoadData
 from dataplot.DataTools import TidyData
 
 def EO_Lessons():
+
+    EO_lesson_list = ['Thermal Camera Analysis', 'Satellite Fire Detection']
+    lesson_options  = [{'label': i.strip(), 'value': i.strip()} for i in EO_lesson_list]
+
     page_layout = html.Div(id ='full_page_container', children =
     [
     html.Div(className = 'page-header', children = [
@@ -14,25 +18,34 @@ def EO_Lessons():
         html.Div(id = 'page-header-holder', children = [html.A('Earth Observation',id = "page-header-text", href = "/dataplot/EO_Lessons")]),
     ]),
     html.Div(className = 'page-body',children = [
-        html.H1(id = 'UploadHolder', children = 'Choose a lesson...'),
+        html.H1(id = 'UploadHolder', children = 'Learn about observing the Earth from space'),
         html.Br(),
         html.P(id = 'EO_lessons_intro', children = [
         'Earth observation info here.']),
     ]),
-    html.Div([
-
-    html.Div(className='EO_Home_Page', children = [
-    html.Div(className='Link_Box', children = [
-        html.Div(className='LinkTIR', children = [
-    dcc.Link('Thermal Camera Quick Play', href='/app1', className='Link_Text'),
-    ])]),
 
 
-    html.Div(className='Link_Box', children = [
-        html.Div(className='LinkLessons',children = [
-    dcc.Link('EO Lessons', href='/app2', className='Link_Text'),
-    ])]),
+    html.Label('Select a lesson:'),
+    dcc.Dropdown(id = 'EO_lesson_choice',
+        multi = False,
+        options = lesson_options,
+        value = 'All'),
+    html.Br(),
 
-    ])])
+    # html.Div([
+    #
+    # html.Div(className='EO_Home_Page', children = [
+    # html.Div(className='Link_Box', children = [
+    #     html.Div(className='LinkTIR', children = [
+    # dcc.Link('Thermal Camera Analysis', href='/app1', className='Link_Text'),
+    # ])]),
+    #
+    #
+    # html.Div(className='Link_Box', children = [
+    #     html.Div(className='LinkLessons',children = [
+    # dcc.Link('Satellite Fire Detection', href='/app2', className='Link_Text'),
+    # ])]),
+    #
+    # ])])
 ])
     return page_layout
