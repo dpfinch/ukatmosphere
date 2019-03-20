f5919fb0c08af13c25007c661539720005a2f850
dpfinch <d.finch@ed.ac.uk>
2019-03-20 14:15:44 +0000

    Added a coastline csv for Australia for ease of plotting.

--- dataplot/EO_Lesson_Tools/Scatter_map.py
+++ dataplot/EO_Lesson_Tools/Scatter_map.py
@@ -5,6 +5,7 @@ import plotly.plotly as py
 import plotly.graph_objs as go
 import pandas as pd
 import numpy as np
+from mpl_toolkits.basemap import Basemap
 
 
 def satellite_scatter(value, removed_310K, cloud_mask, fires_on):
@@ -59,7 +60,7 @@ def satellite_scatter(value, removed_310K, cloud_mask, fires_on):
             lon = fire_locs.Fire_Lons,
             mode = 'markers',
             marker = go.scattermapbox.Marker(
-                symbol = 'fire-station'
+                symbol = 'circle-stroked'
             )
             )
         )
@@ -91,40 +92,7 @@ def satellite_scatter(value, removed_310K, cloud_mask, fires_on):
 
     return fig
 
-def fire_loc_map():
 
-
-    mapbox_access_token = 'pk.eyJ1IjoiZG91Z2ZpbmNoIiwiYSI6ImNqZHhjYnpqeDBteDAyd3FsZXM4ZGdqdTAifQ.xLS22vmqzVYR0SAEDWdLpQ'
-    fire_locs = pd.read_csv('https://raw.githubusercontent.com/dpfinch/ukatmosphere/master/dataplot/assets/fire_locs.csv')
-    print(fire_locs.columns)
-    data = [
-    go.Scattermapbox(
-        lat=fire_locs.Lats,
-        lon=fire_locs.Lons,
-        mode='markers',
-        marker=go.scattermapbox.Marker(
-            size=3,
-            color='red'
-        ),
-    )
-    ]
-
-    layout = go.Layout(
-        autosize=True,
-        hovermode='closest',
-        mapbox=go.layout.Mapbox(
-            accesstoken=mapbox_access_token,
-            bearing=0,
-            center=go.layout.mapbox.Center(
-                lat=-15,
-                lon=142
-            ),
-            pitch=0,
-            zoom=3
-        ),
-    )
-
-    fig = dcc.Graph(id = 'fire_mapbox',
-        figure = {'data':data, 'layout':layout})
-
-    return fig
+def simple_map():
+    # coastline = pd.read_csv('')
+    pass
