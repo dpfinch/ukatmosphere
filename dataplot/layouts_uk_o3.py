39a4b3f83946dcd2ab93ef332664fd507e40958f
dpfinch <d.finxch@ed.ac.uk>
2020-02-05 15:35:14 +0000

    Commented out bootstrap module

--- dataplot/layouts_uk_o3.py
+++ dataplot/layouts_uk_o3.py
@@ -1,7 +1,7 @@
 from random import randint
 import dash_core_components as dcc
 import dash_html_components as html
-import dash_bootstrap_components as dbc
+# import dash_bootstrap_components as dbc
 import dash_daq as daq
 from .server import app
 from dash.dependencies import Output, Input
