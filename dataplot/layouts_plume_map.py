ee3d910d805743a569bbf9814637015af104886f
dpfinch <d.finch@ed.ac.uk>
2021-07-01 12:16:07 +0100

    Restructuring to reduce RAM usage

--- dataplot/layouts_plume_map.py
+++ dataplot/layouts_plume_map.py
@@ -5,35 +5,46 @@ import dash_core_components as dcc
 import dash_html_components as html
 import numpy as np
 import plotly.graph_objects as go
-from .server import app
+from .server import app, queue
 
-def plume_map():
-
-    external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-    token = 'pk.eyJ1IjoiZG91Z2ZpbmNoIiwiYSI6ImNqZHhjYnpqeDBteDAyd3FsZXM4ZGdqdTAifQ.xLS22vmqzVYR0SAEDWdLpQ'
+def load_plume_data():
 
     plume_vertexs = pd.read_csv('https://raw.githubusercontent.com/dpfinch/ukatmosphere/master/dataplot/assets/plume_assets/plume_vertex_list_merged.csv')
-    plume_vertexs = plume_vertexs.replace({np.nan:None})
-    lons = plume_vertexs.Lons.values
-    lats = plume_vertexs.Lats.values
+    # plume_vertexs = plume_vertexs.replace({np.nan:None})
+    plume_lons = plume_vertexs.Lons.values
+    plume_lats = plume_vertexs.Lats.values
+    return plume_lons,plume_lats
+
+def load_ps_data():
 
     pwr_f = 'https://raw.githubusercontent.com/dpfinch/ukatmosphere/master/dataplot/assets/plume_assets/global_power_plant_database.csv'
 
-    ps_df = pd.read_csv(pwr_f)
+    ps_df = pd.read_csv(pwr_f, usecols = ['longitude','latitude','primary_fuel'])
     ps_df = ps_df[ps_df.primary_fuel.isin(['Gas','Oil','Coal'])]
 
+    ps_lons = ps_df.longitude.values
+    ps_lats = ps_df.latitude.values
+
+    return ps_lons, ps_lats
+
+def load_flare_data():
+
     flare_df = pd.read_csv('https://raw.githubusercontent.com/dpfinch/ukatmosphere/master/dataplot/assets/plume_assets/flare_list_merged.csv')
-    flare_df = flare_df.replace({np.nan:None})
+    # flare_df = flare_df.replace({np.nan:None})
     flare_lons = flare_df.Lons.values
     flare_lats = flare_df.Lats.values
 
+    return flare_lons,flare_lats
+
+def render_map(plume_lons,plume_lats,ps_lons,ps_lats,flare_lons,flare_lats):
+    token = 'pk.eyJ1IjoiZG91Z2ZpbmNoIiwiYSI6ImNqZHhjYnpqeDBteDAyd3FsZXM4ZGdqdTAifQ.xLS22vmqzVYR0SAEDWdLpQ'
     fig = go.Figure()
 
     fig.add_trace(go.Scattermapbox(
         mode = "markers",
         name = 'Power Stations',
-        lon = ps_df.longitude,
-        lat = ps_df.latitude,
+        lon = ps_lons,
+        lat = ps_lats,
         hoverinfo = 'none',
         marker = {'color':'red', 'opacity':0.8,
             'size':5}))
@@ -49,8 +60,8 @@ def plume_map():
     fig.add_trace(go.Scattermapbox(
         mode = "lines", fill = "toself",
         name = 'NO<sub>2</sub> Plumes',
-        lon = lons,
-        lat = lats,
+        lon = plume_lons,
+        lat = plume_lats,
         hoverinfo = 'none',
         marker = {'color':'#006391'}))
 
@@ -65,6 +76,18 @@ def plume_map():
         xanchor="left",
         x=0.01
     ))
+
+    return fig
+
+def plume_map():
+
+    external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+    plume_lons,plume_lats = load_plume_data()
+    ps_lons,ps_lats = load_ps_data()
+    flare_lons,flare_lats = load_flare_data()
+
+    fig = render_map(plume_lons,plume_lats,ps_lons,ps_lats,flare_lons,flare_lats)
 
     page_layout = html.Div(id ='full_page_container', children = [
         html.Div([
