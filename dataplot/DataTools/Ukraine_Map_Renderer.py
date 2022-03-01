1439ab5695695db51ab26909b06baf56454ee571
dpfinch <d.finch@ed.ac.uk>
2022-03-01 11:28:21 +0000

    ukraine map changes

--- dataplot/DataTools/Ukraine_Map_Renderer.py
+++ dataplot/DataTools/Ukraine_Map_Renderer.py
@@ -1,6 +1,5 @@
 import plotly.graph_objs as go
 import pandas as pd
-import geopandas as gpd
 import numpy as np
 from datetime import datetime
 import dash_core_components as dcc
@@ -13,15 +12,11 @@ def load_fire_data(app):
     viirs_noaa = pd.read_csv('https://firms.modaps.eosdis.nasa.gov/data/active_fire/noaa-20-viirs-c2/csv/J1_VIIRS_C2_Global_7d.csv')
 
     firms_df = pd.concat([modis_df,viirs_snpp,viirs_noaa])
-    firms_gdf = gpd.GeoDataFrame(firms_df, geometry = gpd.points_from_xy(firms_df.longitude, firms_df.latitude))
 
-    shapefile = app.get_asset_url('ne_10m_admin_0_countries.shp')
+    firms_df = firms_df[(firms_df.latitude > 44) & (firms_df.latitude < 52.5) & (firms_df.longitude > 21.5) & (firms_df.longitude < 40.5)]
 
-    world_gdf = gpd.read_file(shapefile)[['ADMIN', 'ADM0_A3', 'geometry']]
-    ukraine_poly = world_gdf[world_gdf.ADMIN == 'Ukraine']
 
-    ukr_fires = firms_gdf[firms_gdf.geometry.within(ukraine_poly.geometry.iloc[0])]
-    return  ukr_fires.longitude, ukr_fires.latitude
+    return  firms_df.longitude, firms_df.latitude
 
 
 def render_map(app, map_style = 'satellite-streets'):
@@ -41,7 +36,7 @@ def render_map(app, map_style = 'satellite-streets'):
             'size':10}))
 
 
-    fig.update_layout(mapbox = {'style': "stamen-terrain", 'center': {'lon': 30.577133, 'lat': 50.414894}, 'zoom': 12})
+    fig.update_layout(mapbox = {'style': "stamen-terrain", 'center': {'lon': 30.577133, 'lat': 50.414894}, 'zoom': 6})
     fig.update_layout(mapbox_style=map_style,mapbox_accesstoken=token)
     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0}, autosize = True)
     fig.update_layout(legend=dict(
--- dataplot/layouts_ukraine_map.py
+++ dataplot/layouts_ukraine_map.py
@@ -16,45 +16,19 @@ def fire_map():
     page_layout = html.Div(id ='full_page_container', children = [
         html.Div([
         html.Div(className = 'plume-header', children = [
-            html.Div(id = 'home-logo-holder', children = [html.A(id = 'home-logo', href="/")]),
-
-        html.Div(id = 'imgs',children = [
-        html.Img(src = 'https://github.com/dpfinch/ukatmosphere/blob/master/dataplot/assets/plume_assets/plume_0008.png?raw=true', style={'height':'60px', 'width':'60px','padding':'1px','display': 'inline-block'}),
-        html.Img(src = 'https://github.com/dpfinch/ukatmosphere/blob/master/dataplot/assets/plume_assets/plume_0141.png?raw=true', style={'height':'60px', 'width':'60px','padding':'1px','display': 'inline-block'}),
-        html.Img(src = 'https://github.com/dpfinch/ukatmosphere/blob/master/dataplot/assets/plume_assets/plume_0165.png?raw=true', style={'height':'60px', 'width':'60px','padding':'1px','display': 'inline-block'}),
-        html.Img(src = 'https://github.com/dpfinch/ukatmosphere/blob/master/dataplot/assets/plume_assets/plume_0299.png?raw=true', style={'height':'60px', 'width':'60px','padding':'1px','display': 'inline-block'}),
-        html.Img(src = 'https://github.com/dpfinch/ukatmosphere/blob/master/dataplot/assets/plume_assets/plume_0330.png?raw=true', style={'height':'60px', 'width':'60px','padding':'1px','display': 'inline-block'}),
-        html.Img(src = 'https://github.com/dpfinch/ukatmosphere/blob/master/dataplot/assets/plume_assets/plume_0367.png?raw=true', style={'height':'60px', 'width':'60px','padding':'1px','display': 'inline-block'}),
-        ], style={'margin-left': '33%', 'width':'372px','display': 'inline-block'}),
-
-        html.Div(id = 'logos',children = [
-        html.A(href = 'http://www.tropomi.eu', children = [
-            html.Img(src = 'https://github.com/dpfinch/ukatmosphere/blob/master/dataplot/assets/TROPOMI_logo.jpg?raw=true', style={'height':'50px','margin':'10px',})]),
-        html.A(href = 'https://nerc.ukri.org', children = [
-            html.Img(src = 'https://github.com/dpfinch/ukatmosphere/blob/master/dataplot/assets/nerc_logo.png?raw=true', style={'height':'40px','margin':'10px',})]),
-        html.A(href = 'https://www.nceo.ac.uk', children = [
-            html.Img(src = 'https://github.com/dpfinch/ukatmosphere/blob/master/dataplot/assets/nceo_logo.png?raw=true', style={'height':'40px','margin':'10px'})]),
-        ], style={'float': 'right','margin-right':'5px','display': 'inline-block'}),
-
         ],style={'width':'100%', 'height':'70px', 'justify-content': 'center', 'positon':'relative'}),]),
         html.Div(className = 'page-body',children = [
         html.H2('Fires Spotted over Ukraine', style={'textAlign': 'center'}),
         dcc.Markdown('''
-         It is also important to note that the NO<sub>2</sub> data are sensitive to cloud cover and therefore the
-         cloudier a place is (e.g. tropical regions) the less likely there it is for there to be clear
-         observations and therefore the fewer plumes can be observed. Nevertheless, there is no shortage
-         of plumes over the tropics.
+         Fires spotted within the last seven days over Ukraine from the [FIMRS satellite products](https://firms.modaps.eosdis.nasa.gov).
+         These fires could be related to recent military activity in the area.
         ''',dangerously_allow_html = True),
         dcc.Markdown('''
-        More information, including methods and analysis can be found in our [paper](https://amt.copernicus.org/articles/15/721/2022/amt-15-721-2022.html).
-        '''),
-        dcc.Markdown('''
-        Contact: Doug Finch [email](mailto:d.finch@ed.ac.uk) & [Twitter](https://twitter.com/douglasfinch)
-        [<img src="https://github.com/dpfinch/ukatmosphere/blob/master/dataplot/assets/twitter_logo.png?raw=true" width="20px"/>](https://twitter.com/douglasfinch)''',
-        dangerously_allow_html = True),
+         It is important to note that this satellite product does not capture all fires and will have some false detections. Also many of these fires will not be related to military activity.
+        ''',dangerously_allow_html = True),
         html.Hr(),
         dcc.Loading(id='loading_plume_map',children=[
-        html.Div(id = 'fire_map_holder',style={'width': '100%', 'height': '90vh'})],
+        html.Div(id = 'fire_map_holder',style={'width': '90%', 'height': '90vh'})],
         type="graph"),
         html.Br(),
         daq.ToggleSwitch(id = 'fire_map_toggle', value = True,
